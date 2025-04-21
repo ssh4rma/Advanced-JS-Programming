68ef087be2ba74724dc657b58b797c6677719cc2
Shubham <shubham.sharma4390@gmail.com>
2025-04-21 17:36:49 +0530

    selection sort problem

--- Searching and sorting/sorting-problems.js
+++ Searching and sorting/sorting-problems.js
@@ -18,3 +18,26 @@ const arr = [
   { id: 2 }
 ];
 console.log(bubbleSort(arr));
+
+//selection sort
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j].id < arr[minIdx].id) {
+        minIdx = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[minIdx];
+    arr[minIdx] = temp;
+  }
+  return arr;
+}
+const arr = [
+  { id: 4 },
+  { id: 2 },
+  {},
+  { id: 1 }
+];
+console.log(selectionSort(arr));
