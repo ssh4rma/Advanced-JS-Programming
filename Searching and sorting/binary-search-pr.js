1cab416aa67d273f26976a2dd9739ff181e5263f
Shubham <shubham.sharma4390@gmail.com>
2025-04-21 17:33:20 +0530

    binary seach problem

--- Searching and sorting/binary-search-pr.js
+++ Searching and sorting/binary-search-pr.js
@@ -20,3 +20,28 @@ const arr = [
   { id: 2 }
 ];
 console.log(binarySearch(arr, 2));
+
+
+//problem 2
+function binarySearch2(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid].value === target) {
+      return mid;
+    } else if (arr[mid].value > target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+const arr = [
+  { value: 10 },
+  { value: 20 },
+  { value: 30 },
+  { value: 40 }
+];
+console.log(binarySearch2(arr, 20));
