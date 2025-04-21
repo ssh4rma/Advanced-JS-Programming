ecb1f363bc3a6a3cb5f31ec8447b00a0d16266d8
Shubham <shubham.sharma4390@gmail.com>
2025-04-21 11:35:12 +0530

    implemented bubble sort algirhtm and instantiated one object and called the method of binary seach for seaching an element after sorting the given array

--- Searching and sorting/bubble-sort.js
+++ Searching and sorting/bubble-sort.js
@@ -23,4 +23,19 @@ let arr = [12, 11, 13, 5, 6];
 let x = 11;
 
 
+function bubble_sort(arr) {
+  let n = arr.length;
 
+  for(let i = 0; i < n; ++i) {
+    for(let j = i + 1; j < n - i; ++j) {
+      if(arr[j] < arr[i]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubble_sort(arr));
+const s = new Sorting;
+console.log(s.binary_search(arr, x));
