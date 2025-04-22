97e0c7a1c61b314d2c2af9875e727e7f77ed7211
Shubham <shubham.sharma4390@gmail.com>
2025-04-22 14:14:24 +0530

    function to add element at the end of the list

--- Linked List/singly-linked-list.js
+++ Linked List/singly-linked-list.js
@@ -5,4 +5,31 @@ class Node {
   }
 }
 const head = new Node(1);
-console.log(head); //this will return an object and then I can acess the value of head and next pointer in that
+// console.log(head); //this will return an object and then I can acess the value of head and next pointer in that
+
+
+function add_element_at_end(head, val) {
+  let temp = head;
+  while(temp.next) {
+    temp = temp.next;
+  }
+
+  let newNode = new Node(val);
+  temp.next = newNode;
+  return head; 
+}
+
+function printList(head) {
+  let temp = head; 
+  while(temp.next) {
+    console.log(temp.val);
+    temp = temp.next;
+  }
+}
+
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+printList(head);
