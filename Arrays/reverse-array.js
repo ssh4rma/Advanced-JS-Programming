0ea8a83d71dd9c3e6443bb86dc9de38f867e295f
Shubham <shubham.sharma4390@gmail.com>
2025-04-22 10:19:17 +0530

    reverse array: method 3

--- Arrays/reverse-array.js
+++ Arrays/reverse-array.js
@@ -7,3 +7,13 @@ console.log(arr); //array has modified
 //method 2:
 console.log(arr.toReversed()); //don't hamper the original array
 console.log(arr); //original array is not modified
+
+//method 3: (hard code)
+
+console.log(arr); //5, 4, 3, 2, 1
+let n = arr.length;
+for(let i = 0; i < n / 2; ++i) {
+  [arr[i], arr[n-i-1]] = [arr[n-i-1], arr[i]];
+}
+
+console.log(arr); // 1, 2, 3, 4, 5
