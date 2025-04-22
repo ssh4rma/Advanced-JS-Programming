4233660377c6fde6c574b6362a8331ba6defcdda
Shubham <shubham.sharma4390@gmail.com>
2025-04-22 12:57:02 +0530

    created node class

---
+++ Linked List/singly-linked-list.js
@@ -0,0 +1,8 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+const head = new Node(1);
+console.log(head); //this will return an object and then I can acess the value of head and next pointer in that
