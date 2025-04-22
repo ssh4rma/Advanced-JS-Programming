8ea1e53f6c4716737b94055b2ca0ee78453aa2f8
Shubham <shubham.sharma4390@gmail.com>
2025-04-22 10:17:01 +0530

    reverse arary: method 2

--- Arrays/reverse-array.js
+++ Arrays/reverse-array.js
@@ -4,3 +4,6 @@ let arr = [1, 2, 3, 4, 5];
 console.log(arr.reverse()); //inplace reversal
 console.log(arr); //array has modified
 
+//method 2:
+console.log(arr.toReversed()); //don't hamper the original array
+console.log(arr); //original array is not modified
