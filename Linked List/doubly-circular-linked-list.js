db6423ddf4e33fded90e8b0293d94b71ebebd223
Shubham <shubham.sharma4390@gmail.com>
2025-04-22 16:39:16 +0530

    doubly circular linked list insertion at the start

--- Linked List/doubly-circular-linked-list.js
+++ Linked List/doubly-circular-linked-list.js
@@ -5,3 +5,44 @@ class Node {
     this.prev = null;
   }
 }
+
+function insertAtStart(head, val) {
+  const newNode = new Node(val);
+  if(!head) {
+    newNode.next = newNode;
+    newNode.prev = null;
+    return newNode;
+  }
+
+  let tail = head.prev;
+
+  newNode.next = head;
+  newNode.prev = tail;
+
+  head.prev = newNode;
+  tail.next = newNode;
+
+  return newNode;
+}
+
+function printList(head) {
+  if(!head) return;
+  let temp = head;
+  
+  do {
+    console.log(temp.val);
+    temp = temp.next;
+  } while(temp !== head);
+}
+
+let head = new Node(1);
+head.next = head;
+head.prev = head;
+
+head = insertAtStart(head, 2);
+head = insertAtStart(head, 3);
+head = insertAtStart(head, 4);
+head = insertAtStart(head, 5);
+head = insertAtStart(head, 6);
+
+printList(head); 
