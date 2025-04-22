fcb756d0944bba42c2fdbe141a300309399268ae
Shubham <shubham.sharma4390@gmail.com>
2025-04-22 11:02:51 +0530

    remove duplicates from sorted array

--- Arrays/remove-duplicates-from-sorted-array.js
+++ Arrays/remove-duplicates-from-sorted-array.js
@@ -1,4 +1,4 @@
-let arr = [1, 1, 2, 2, 3];
+let arr = [0,0,1,1,1,2,2,3,3,4];
 let n = arr.length;
 
 //method 1: using set container
@@ -15,3 +15,15 @@ for(let i of st) {
 
 console.log(res);
 //time complexity: O(n) and space complexity: O(n + n) 
+
+//method 2: 2 pointer
+
+if (n === 0) return [];
+
+let k = 1;  
+for (let i = 1; i < n; i++) {
+  if (arr[i] !== arr[i - 1]) {
+    arr[k++] = arr[i];
+  }
+}
+console.log(arr.slice(0, k));
