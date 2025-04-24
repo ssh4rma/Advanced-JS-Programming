6b135f57ecec2113d2746e964451b7d4fc8f2716
Shubham <shubham.sharma4390@gmail.com>
2025-04-24 15:05:50 +0530

    enqueue method of queue

--- queue.js
+++ queue.js
@@ -4,3 +4,26 @@ class Node {
     this.next = null;
   }
 }
+
+class Queue {
+  constructor(head) {
+    this.head = null;
+  }
+
+  enq(val) {
+    const newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      return this.head;
+    }
+    let temp = this.head;
+    while(temp.next) {
+      temp = temp.next;
+    }
+
+    temp.next = newNode;
+    newNode.next = null;
+    
+    return this.head;
+  }
+}
