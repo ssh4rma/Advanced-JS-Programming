79d54055502db792895d52d46ed73afb6df0572a
Shubham <shubham.sharma4390@gmail.com>
2025-04-28 14:59:03 +0530

    composition function in JS

--- JS questions.js
+++ JS questions.js
@@ -36,3 +36,14 @@ var createCounter = function(init) {
     }
   }
 };
+
+//composition funciton
+var compose = function(functions) {
+    
+  return function(x) {
+      for(let i = functions.length - 1; i >= 0; --i) {
+          x = functions[i](x);
+      }
+      return x; 
+  }
+};
