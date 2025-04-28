131d0ea0232c4f2afcba50ba75570560154767dd
Shubham <shubham.sharma4390@gmail.com>
2025-04-28 14:57:04 +0530

    test function for detecting if the value is true or not

--- JS questions.js
+++ JS questions.js
@@ -5,3 +5,18 @@ var createCounter = function(n) {
       return cnt++;
   };
 };
+
+//Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+var expect = function(val) {
+  return {
+    toBe: function(value) {
+      if(val === value) return true;
+      else throw new Error("Not Equal"); 
+    }, 
+    notToBe: function(value) {
+      if(val !== value) return true;
+      else throw new Error("Equal"); 
+    }
+  }
+};
