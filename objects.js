65cd17f5c6674766beee93701fa09aeacb135620
Shubham <shubham.sharma4390@gmail.com>
2025-04-30 12:15:01 +0530

    configurable: false example implementation

--- objects.js
+++ objects.js
@@ -3,13 +3,28 @@ let user = {
   age: '22'
 }
 
+
+// Object.defineProperty(user, "name", { writable: false });
+
+// let descriptor2 = Object.getOwnPropertyDescriptor(user, 'name');
+
+// user.name = 'pete';
+// console.log(descriptor2); 
+// console.log(user.name); 
+
+//non configurable
+Object.defineProperty(user, 'name', {
+  writable: true, 
+  configurable: false
+});
+// let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+// console.log(descriptor); 
+
+
+user.name = 'rajesh';
+console.log(user.name); 
+Object.defineProperty(user, 'name', {
+  writable: false
+});
 let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
 console.log(descriptor); 
-
-Object.defineProperty(user, "name", { writable: false });
-
-let descriptor2 = Object.getOwnPropertyDescriptor(user, 'name');
-
-user.name = 'pete';
-console.log(descriptor2); 
-console.log(user.name); 
