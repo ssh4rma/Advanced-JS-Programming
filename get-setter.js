04737cd97ca2b204144c815fa8ef041d35afe6fd
Shubham <shubham.sharma4390@gmail.com>
2025-04-30 12:42:49 +0530

    getter and setter updated code, and accessing the access descriptor using Object.getOwnPropertyDescriptor() method

--- get-setter.js
+++ get-setter.js
@@ -1,17 +1,21 @@
 let users = {
   name : 'shubham', 
-  age : 22,
+  age : 22
+};
 
-  get Data() {
+Object.defineProperty(users, 'data', {
+  get() {
     return `The name of the user is ${this.name} and their age is ${this.age}`;
   },
 
-  set updateData(val) {
+  set(val) {
     this.age = val;
   } 
-};
+});
 
 console.log(users.Data);
 users.updateData = 23;
 // console.log(users);
 console.log(users.Data);
+
+console.log(Object.getOwnPropertyDescriptor(users, 'data'));  
