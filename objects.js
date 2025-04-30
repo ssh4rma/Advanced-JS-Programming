63f9c5f5a64fd4c57c6265fc08158d1da59af4c3
Shubham <shubham.sharma4390@gmail.com>
2025-04-30 10:37:41 +0530

    defineProperty() method for modifying the descriptor value

--- objects.js
+++ objects.js
@@ -6,3 +6,10 @@ let user = {
 let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
 console.log(descriptor); 
 
+Object.defineProperty(user, "name", { writable: false });
+
+let descriptor2 = Object.getOwnPropertyDescriptor(user, 'name');
+
+user.name = 'pete';
+console.log(descriptor2); 
+console.log(user.name); 
