59e62ec4ce896e5105709de81611315ffc964445
Shubham <shubham.sharma4390@gmail.com>
2025-04-30 14:37:23 +0530

    constructor function of array and powerArray are same

--- extending-build-in-class.js
+++ extending-build-in-class.js
@@ -8,3 +8,5 @@ const arr = new PowerArray(1,2,3,5,3,23,21);
 console.log(arr.isEmpty());
 const filteredArray = arr.filter((val) => val <= 10);
 console.log(filteredArray.isEmpty()); 
+
+console.log(arr.constructor === PowerArray); //true
