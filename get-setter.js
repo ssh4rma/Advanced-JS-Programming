3341c1339f83d68573dcdcff2f381d5071c834a6
Shubham <shubham.sharma4390@gmail.com>
2025-04-30 12:21:20 +0530

    setter implementation in an object

--- get-setter.js
+++ get-setter.js
@@ -4,8 +4,14 @@ let users = {
 
   get Data() {
     return `The name of the user is ${this.name} and their age is ${this.age}`;
-  }
+  },
+
+  set updateData(val) {
+    this.age = val;
+  } 
 };
 
 console.log(users.Data);
-
+users.updateData = 23;
+// console.log(users);
+console.log(users.Data);
