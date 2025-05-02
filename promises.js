5f23bb27ddff11cc385092e1640d8e097876be63
Shubham <shubham.sharma4390@gmail.com>
2025-05-02 10:50:05 +0530

    .then() function syntax to deal with error alone

--- promises.js
+++ promises.js
@@ -9,3 +9,7 @@ let promise = new Promise((res, rej) => {
 let rejPromise = new Promise((res, rej) => {
   setTimeout(() => rej(new Error ('error'), 1000));
 }).catch(err => console.log(err));
+
+let rejPromise2 = new Promise((res, rej) => {
+  setTimeout(() => rej(new Error ('error'), 1000));
+}).then(null, err => console.log(err));
