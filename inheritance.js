2767c9ee89069081b4a13d91771ce7c47fe3bcb0
Shubham <shubham.sharma4390@gmail.com>
2025-05-02 16:44:05 +0530

    inheritance using prototype using another method

--- inheritance.js
+++ inheritance.js
@@ -11,3 +11,11 @@ const print = {
 
 Object.setPrototypeOf(print, obj);
 console.log(print.greet());
+
+//second method
+const print2 = Object.create(obj);
+print2.greet = function() {
+  return `The name of the user is ${this.name} and the age of the user is ${this.age}`;
+}
+
+console.log(print2.greet());
