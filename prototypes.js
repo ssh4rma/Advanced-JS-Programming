af729b7f134aeb39de0111ad6aeea2455622142b
Shubham <shubham.sharma4390@gmail.com>
2025-05-02 16:26:39 +0530

    assigned prototype to the new object using Object.create() method

--- prototypes.js
+++ prototypes.js
@@ -9,3 +9,7 @@ console.log(obj.greet());
 console.log(obj.__proto__);
 console.log(Object.getPrototypeOf(obj));
 console.log(obj.__proto__ === Object.getPrototypeOf(obj));
+
+
+const newObj = Object.create(obj);
+console.log(newObj.greet());
