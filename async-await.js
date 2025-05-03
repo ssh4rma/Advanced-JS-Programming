721c6afc4dd1f377a10db063e3e47f24265d3ebb
Shubham <shubham.sharma4390@gmail.com>
2025-05-03 10:59:04 +0530

    async await function example

--- async-await.js
+++ async-await.js
@@ -3,3 +3,16 @@ async function f() {
 }
 
 f().then((res) => console.log(res));
+
+async function helper() {
+  let pr = new Promise((res, rej) => {
+    setTimeout(() => {
+      res('Promise resolved!');
+    }, 1000);
+  });
+
+  let res = await pr; 
+  console.log(res); 
+}
+
+helper(); 
