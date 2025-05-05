b4f357072aef3231ced33579edaa6b69faa43c42
Shubham <shubham.sharma4390@gmail.com>
2025-05-05 10:52:57 +0530

    Refactor promise handling to ensure all promises resolve correctly and log results

--- promise-api.js
+++ promise-api.js
@@ -1,5 +1,22 @@
 Promise.all([
   new Promise(res => setTimeout(() => res(console.log('p1 resolved')), 1000)),
   new Promise(res => setTimeout(() => res(console.log('p2 resolved'))), 2000),
-  new Promise(res, rej => rej(new Error('p3 not resolved')))
-]).catch(rej => console.log(rej));
+  new Promise(res => res('p3 is resolved'))
+]).then((res) => console.log(res));
+
+Promise.all([
+  new Promise(res => setTimeout(() => {
+    console.log('p1 resolved');
+    res('Result 1');
+  })),
+  new Promise(res => setTimeout(() => {
+    console.log('p2 resolved');
+    res('Result 2');
+  })),
+  new Promise(res => setTimeout(() => {
+    console.log('p3 resolved');
+    res('Result 3');
+  })),
+]).then(results => {
+  console.log('All promises resolved:', results);
+});
