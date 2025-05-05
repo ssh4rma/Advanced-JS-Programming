1567163af914d7eba52d1dbde378150b64491761
Shubham <shubham.sharma4390@gmail.com>
2025-05-05 11:09:55 +0530

    Add additional Promise.all() example to demonstrate promise resolution

--- promise-api.js
+++ promise-api.js
@@ -20,3 +20,9 @@ Promise.all([
 ]).then(results => {
   console.log('All promises resolved:', results);
 });
+
+Promise.all([
+  new Promise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+  new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+  new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
+]).then(res => console.log(res)); 
