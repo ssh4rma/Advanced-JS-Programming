5fbae48414939175272e6ab0a70f3d6f850da2ad
Shubham <shubham.sharma4390@gmail.com>
2025-05-05 11:42:22 +0530

    Add Promise.race example to demonstrate promise resolution and error handling

--- promise-api.js
+++ promise-api.js
@@ -57,3 +57,10 @@ Promise.allSettled(urls.map((url) => fetch(url)))
       }
     })
   })
+
+Promise.race([
+  new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000)),
+  new Promise((resolve, reject) => setTimeout(() => reject(new Error("Whoops!")), 2000)),
+  new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000))
+])
+  .then((res) => console.log(res));
