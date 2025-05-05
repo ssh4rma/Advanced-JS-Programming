de27de52f176f4e06111b5e442df110c40f1465b
Shubham <shubham.sharma4390@gmail.com>
2025-05-05 11:55:16 +0530

    Promise.any() API example

--- promise-api.js
+++ promise-api.js
@@ -64,3 +64,10 @@ Promise.race([
   new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000))
 ])
   .then((res) => console.log(res));
+
+Promise.any([
+  new Promise((resolve, reject) => setTimeout(() => reject(new Error("Whoops!")), 1000)),
+  new Promise((resolve, reject) => setTimeout(() => resolve(1), 2000)),
+  new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000))
+])
+  .then((res) => console.log(res));
